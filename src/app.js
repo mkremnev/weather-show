fd974f51508456ae01013d92bd41245ca9c443f9
Maxim Kremnev <m.kremnev@netlinux.ru>
2022-04-07 15:50:46 +0300

    Added app styles

--- src/app.js
+++ src/app.js
@@ -3,10 +3,5 @@ import './styles/main.scss';
 export const App = () => {
   const container = document.createElement('div');
 
-  const button = document.createElement('button');
-  button.innerText = 'Нажми на меня';
-  button.addEventListener('click', () => console.log('click'));
-
-  container.appendChild(button);
   return container;
 };
