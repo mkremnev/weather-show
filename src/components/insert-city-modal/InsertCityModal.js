908c00a58044a0310509a9f1d967712995a7db8d
Maxim Kremnev <m.kremnev@netlinux.ru>
2022-04-11 12:04:41 +0300

    Created list cities

--- src/components/insert-city-modal/InsertCityModal.js
+++ src/components/insert-city-modal/InsertCityModal.js
@@ -1,12 +1,15 @@
 import { observer } from '@/app';
 import CityForm from './CityForm';
+import ListCities from './ListCities';
 
 const InsertCityModal = () => {
   const cityForm = CityForm();
+  const listCities = ListCities();
   const modal = document.createElement('div');
   modal.classList.add('insert-city-modal');
 
   modal.insertAdjacentElement('afterbegin', cityForm);
+  modal.insertAdjacentElement('beforeend', listCities);
 
   observer.subscribe((type) => {
     if (type === 'cities') {
